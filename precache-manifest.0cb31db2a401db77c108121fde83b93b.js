1c31a80d35230690b8ab5ec69eae94e6cdd8aea6
Marcin Gol <golmarcin@gmail.com>
2019-09-19 20:51:12 +0200

    Updates

---
+++ precache-manifest.0cb31db2a401db77c108121fde83b93b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65419c4da2c1f0c79c9004d259833a09",
+    "url": "/plane-push/index.html"
+  },
+  {
+    "revision": "4350007e2c0ea5f030a7",
+    "url": "/plane-push/static/css/main.196389cb.chunk.css"
+  },
+  {
+    "revision": "308fe1b4513a9400c888",
+    "url": "/plane-push/static/js/2.89f60859.chunk.js"
+  },
+  {
+    "revision": "4350007e2c0ea5f030a7",
+    "url": "/plane-push/static/js/main.e00b3307.chunk.js"
+  },
+  {
+    "revision": "b6739241e709f12996aa",
+    "url": "/plane-push/static/js/runtime~main.61546226.js"
+  }
+]);
